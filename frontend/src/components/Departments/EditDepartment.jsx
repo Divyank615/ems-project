ba2b2d6790c5c32a37c69d618acda7e376b0ddcd
Divyank615 <divyankchoudhary815@gmail.com>
2025-06-01 17:54:22 +0530

    Update EditDepartment.jsx

--- frontend/src/components/Departments/EditDepartment.jsx
+++ frontend/src/components/Departments/EditDepartment.jsx
@@ -20,7 +20,7 @@ const EditDepartment = () => {
        const fetchDepartments =async()=>{
         setDepLoading(true)
         try{
-          const response =await axios.get(`http://localhost:3000/api/department/${id}`,{
+          const response =await axios.get(`https://ems-project-backend.onrender.com/api/department/${id}`,{
             headers: {
               "Authorization": `Bearer ${localStorage.getItem("token")}`
               }
@@ -44,7 +44,7 @@ const EditDepartment = () => {
       const handleSubmit = async(e) => {
         e.preventDefault();
        try{
-           const response =await axios.put(`http://localhost:3000/api/department/${id}`,department,{
+           const response =await axios.put(`https://ems-project-backend.onrender.com/api/department/${id}`,department,{
             headers: {
                 "Authorization" : `Bearer ${localStorage.getItem("token")}`,
              }
