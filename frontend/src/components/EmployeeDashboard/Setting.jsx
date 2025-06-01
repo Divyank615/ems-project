404d681c44f28892d32884f666064ece9fa7405d
Divyank615 <divyankchoudhary815@gmail.com>
2025-06-01 17:55:01 +0530

    Update Setting.jsx

--- frontend/src/components/EmployeeDashboard/Setting.jsx
+++ frontend/src/components/EmployeeDashboard/Setting.jsx
@@ -103,7 +103,7 @@ const Setting = () => {
         
        }else{
          try{
-           const response =await axios.put(`http://localhost:3000/api/setting/change-password`,setting,{
+           const response =await axios.put(`https://ems-project-backend.onrender.com/api/setting/change-password`,setting,{
             headers: {
                 "Authorization" : `Bearer ${localStorage.getItem("token")}`,
             }
