9bebbd4545937f65bf8860005e25b3923a64df14
Divyank615 <divyankchoudhary815@gmail.com>
2025-06-01 18:03:28 +0530

    Update EmployeeHelper.jsx

--- frontend/src/utilities/EmployeeHelper.jsx
+++ frontend/src/utilities/EmployeeHelper.jsx
@@ -46,7 +46,7 @@ export const columns =[
  export const fetchDepartments =async()=>{
     let departments 
     try{
-      const response =await axios.get('http://localhost:3000/api/department',{
+      const response =await axios.get('https://ems-project-backend.onrender.com/api/department',{
         headers: {
           "Authorization": `Bearer ${localStorage.getItem("token")}`
           }
@@ -69,7 +69,7 @@ export const columns =[
     export const getEmployees =async(id)=>{
     let employees;
     try{
-      const response =await axios.get(`http://localhost:3000/api/employee/department/${id}`,{
+      const response =await axios.get(`https://ems-project-backend.onrender.com/api/employee/department/${id}`,{
         headers: {
           "Authorization": `Bearer ${localStorage.getItem("token")}`
           }
